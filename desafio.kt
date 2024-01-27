d120278a919bc058820ed1fc45e4c88291085607
RobertoPellozo <148584990+RobertoPellozo@users.noreply.github.com>
2024-01-26 22:27:50 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,12 +1,12 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { Easy, Mediun, Hard }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 20)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
@@ -16,6 +16,10 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
+    val nivel = Nivel.Hard
+    val usuario = "Roberto"
+    ConteudoEducacional("Kotlin")
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+//Devido a minha dificuldade inicial ainda estou entendedo o conteudo e não consegui evoluir
